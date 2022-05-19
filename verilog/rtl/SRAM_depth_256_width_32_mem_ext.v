4e72a5c0e3869ec6431028441a4c111763a5c39b
Nikola Petrovic <nikola.petrovic@novelic.com>
2022-05-19 16:57:06 +0200

    Added additional logic to SRAM wrapper in order to work with FFT as expected

--- verilog/rtl/SRAM_depth_256_width_32_mem_ext.v
+++ verilog/rtl/SRAM_depth_256_width_32_mem_ext.v
@@ -9,36 +9,36 @@ module SRAM_depth_256_width_32_mem_ext(
   input         W0_clk,
   input  [31:0] W0_data
 );
+  
+  reg  [31:0] r0_data_temp;
+  reg r_R0_en;
 
-  wire  [7:0] w_R0_addr;
-  wire        w_R0_clk;
-  wire        w_R0_en;
+  reg  [31:0] r_R0_data;
   wire [31:0] w_R0_data;
-  wire  [7:0] w_W0_addr;
-  wire        w_W0_en;
-  wire        w_W0_clk;
-  wire [31:0] w_W0_data;
 
-  assign w_R0_addr = R0_addr;
-  assign w_R0_clk  = R0_clk;
-  assign w_R0_en   = R0_en;
-  assign R0_data   = w_R0_data;
-  assign w_W0_addr = W0_addr;
-  assign w_W0_en   = W0_en;
-  assign w_W0_clk  = W0_clk;
-  assign w_W0_data = W0_data;
+  assign R0_data = r0_data_temp;
+
+  always @(posedge R0_clk) begin
+    r_R0_en <= R0_en;
+    if (r_R0_en == 1'b1) r_R0_data <= w_R0_data;
+  end
+
+  always @(*) begin
+    if (r_R0_en == 1'b0) r0_data_temp = r_R0_data;
+    else r0_data_temp = w_R0_data;
+  end
 
   sky130_sram_1kbyte_1rw1r_32x256_8 #(.VERBOSE(0)) sram (
-    .clk0  (w_W0_clk),  // input  - clock
-    .csb0  (!w_W0_en),  // input  - active low chip select
-    .web0  (!w_W0_en),  // input  - active low write control
+    .clk0  (W0_clk),    // input  - clock
+    .csb0  (!W0_en),    // input  - active low chip select
+    .web0  (!W0_en),    // input  - active low write control
     .wmask0(4'b1111),   // input  - write mask
-    .addr0 (w_W0_addr), // input  - addr
-    .din0  (w_W0_data), // input  - data
+    .addr0 (W0_addr),   // input  - addr
+    .din0  (W0_data),   // input  - data
     .dout0 (),          // output - data
-    .clk1  (w_R0_clk),  // input  - clock
-    .csb1  (!w_R0_en),  // input  - active low chip select
-    .addr1 (w_R0_addr), // input  - addr
+    .clk1  (R0_clk),    // input  - clock
+    .csb1  (!R0_en),    // input  - active low chip select
+    .addr1 (R0_addr),   // input  - addr
     .dout1 (w_R0_data)  // output - data
   );
 endmodule
