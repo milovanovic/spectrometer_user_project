1c80617dcdc417329ab15d6d35715f17edceb180
Nikola Petrovic <nikola.petrovic@novelic.com>
2022-06-05 13:49:43 +0200

    Added clk to gpio 7

--- verilog/dv/hyperspace/hyperspace.c
+++ verilog/dv/hyperspace/hyperspace.c
@@ -52,38 +52,41 @@ void main()
         count = count + 1;
     }
 
-    // Configure IO pins for in and out AXI4-Stream
-    reg_mprj_io_8  =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_9  =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_10 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_11 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_12 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_13 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_14 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_15 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_16 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_17 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_18 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_19 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_20 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_21 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_22 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_23 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_24 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_25 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_26 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
+    // CLK
+    reg_mprj_io_7  = GPIO_MODE_USER_STD_OUTPUT;
 
-    reg_mprj_io_27 =  GPIO_MODE_USER_STD_OUTPUT;
-    reg_mprj_io_28 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_29 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_30 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_31 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_32 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_33 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_34 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_35 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_36 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
-    reg_mprj_io_37 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
+    // Configure IO pins for in and out AXI4-Stream
+    reg_mprj_io_8  = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_9  = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_15 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_16 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_17 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_18 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_19 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_20 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_21 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_22 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_23 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_24 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_25 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_26 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+
+    reg_mprj_io_27 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_28 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_29 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_30 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_31 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_32 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_33 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_34 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_35 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_36 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_37 = GPIO_MODE_USER_STD_INPUT_NOPULL;
 
     /* Apply configuration */
     reg_mprj_xfer = 1;
